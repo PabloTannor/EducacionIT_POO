b91b616b42daf0ad45fe3690237d2740317be8d2
Pablo Tannor <pablotannor@gmail.com>
2024-05-05 12:25:15 -0300

    Primera clase POO

---
+++ Clase1/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase1
+{
+    public class Cliente
+    {
+        int IdCliente;
+        public string Nombre;
+        string Apellido;
+        string NroDocumento;
+        string TipoDocumento;
+        string TpoPersona;
+        int Edad;
+        string Direccion;
+        string Telefono;
+
+    }
+}
---
+++ Clase1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Clase1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente cliente1 = new Cliente();
+            cliente1.Nombre = "Pepe";
+        }
+    }
+}
